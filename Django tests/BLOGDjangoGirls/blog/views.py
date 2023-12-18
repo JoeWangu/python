b6ec48c0d1616cce4795ab3fe321ec0ea1c73c70
Joel-009 <79811019+Joel-009@users.noreply.github>
2023-12-18 14:44:31 +0300

    18-Dec-2023

---
+++ Django tests/BLOGDjangoGirls/blog/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Post
+from django.utils import timezone
+from .forms import PostForm
+from django.contrib import messages
+
+# Create your views here.
+
+
+def post_list(request):
+    # posts = Post.objects.filter(
+    #     published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/post_detail.html', context)
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            # message = messages('Saved Successfully')
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    context = {
+        'form': form,
+        # 'message':message,
+    }
+    return render(request, 'blog/post_new.html', context)
+
+def post_edit(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    context = {
+        'form': form,
+    }
+    return render(request, 'blog/post_edit.html', context)
