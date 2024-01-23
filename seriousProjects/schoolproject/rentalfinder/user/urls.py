7f5dba88eafc9e3ffec1c4a94471832eb5871ae5
Joel-009 <79811019+Joel-009@users.noreply.github>
2024-01-23 13:48:46 +0300

    Removed a file that's now in .gitignore

--- seriousProjects/schoolproject/rentalfinder/user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from user.views.api import CreateUser, LoginUser
-
-urlpatterns = [
-    path('', LoginUser.as_view(), name='login-api'),
-    path('create-user-api/', CreateUser.as_view(), name='create-user-api'),
-]
