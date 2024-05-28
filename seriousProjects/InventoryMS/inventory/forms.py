3fc7098dd04bcbe511dabd661460178469785c47
joewangu <joewangu2021@outlook.com>
2024-05-28 11:12:26 +0300

    remove files

--- seriousProjects/InventoryMS/inventory/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-
-# class ProductForm(ModelForm):
-#     class Meta:
-#         model = Product
-#         fields = ['name', 'model_number', 'price', 'quantity_in_stock', 'dimensions', 'image']
-#     image = forms.ChoiceField(choices=Product.image_choices)
--- seriousProjects/InventoryMS/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class UserPermissions(models.Model):
-    is_staff = models.BooleanField(default=False)
-    is_manual_worker = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
-    is_gateway_mall_staff = models.BooleanField(default=False)
-    is_two_rivers_mall_staff = models.BooleanField(default=False)
